9f8f01db53d517ae371abe02f9488bc908cd9747
Rivka Benyowitz <rbenyowitz@wits.edu>
2024-06-27 17:54:19 -0400

    Select menu and Card working... Button not yet

--- cookie-cookbook-app/src/Pages/Update.jsx
+++ cookie-cookbook-app/src/Pages/Update.jsx
@@ -3,40 +3,68 @@ import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
 
 const Update = () => {
-    const [recipeList, setRecipeList] = useState([{key:"Banana", value:{1:"Flour", 2:"Sugar"}}, {key:"Chocolate", value:{1:"Oil", 2:"Eggs"}}])
-    const [chosenRecipe, setChosenRecipe] = useState({})
+    const [recipeList, setRecipeList] = useState([{"Banana":[{1:"Flour"}, {2:"Sugar"}]}, {"Chocolate":[{1:"Oil"}, {2:"Eggs"}]}])
+    const [chosenRecipe, setChosenRecipe] = useState({"Sample Recipe":[{1:"something sweet"}, {2:"something spicy"}]})
 
     const handleSelectChange = (event) => {
-        setChosenRecipe(event.target.value);
+        const selectedKey = event.target.value;
+        const recipe = recipeList.find(item => Object.keys(item)[0] === selectedKey);
+        setChosenRecipe(recipe);
+    }
 
-        return (
-            <>
-                <Card style={{ width: '50%' }}>
-                <Card.Header>{Object.keys(chosenRecipe)[0]}</Card.Header>
-                <ListGroup variant="flush">
-                    {Object.valules(chosenRecipe).map((ingredient) => (
-                    <option key = {ingredient.key} value={recipe} >{recipe.key}</option>)    
-                    )}
-                    <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                    <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-                </ListGroup>
-                </Card>
-            </>
-        )
+    const handleButtonClick = (event) => {
+        
+
+    }
+
+    const handleFormControlChange = (event) => {
+        const addition = event.target.value;
+        const additionKey = Object.values(chosenRecipe).length + 2
+        const recipeName = Object.keys(chosenRecipe)[0]
+        const newRecipeList = [...Object.values(chosenRecipe), {additionKey:addition}]
+        const updatedRecipe = {recipeName:newRecipeList}
+
+        console.log("addition: ", addition);
+        console.log("additionKey: ", additionKey);
+        console.log("recipeName: ", recipeName);
+        console.log("newRecipeLsit: ", newRecipeList);
+
+
+        setChosenRecipe(updatedRecipe);
     }
 
     return (
         <>
-            <Form.Select aria-label="Default select example" size="lg" onChange = "handleSelectChange(event)">
-                <option>Choose A Recipe</option>
-                {recipeList.map((recipe) => (
-                    <option key = {recipe.key} value={recipe} >{recipe.key}</option>)    
+            <Form.Select aria-label="Default select example" size="lg" onChange = {(event) => handleSelectChange(event)}>
+                <option disabled>Choose A Recipe</option>
+                {recipeList.map((recipe, index) => (
+                    <option key = {index} value={Object.keys(recipe)[0]} >{Object.keys(recipe)[0]}</option>)    
                 )}
 
             </Form.Select>
+
+            <Card style={{ width: '50%' }}>
+                <Card.Header>{Object.keys(chosenRecipe)[0]}</Card.Header>
+                <ListGroup variant="flush">
+                    {chosenRecipe[Object.keys(chosenRecipe)[0]].map((ingredients, index) => (
+                        <ListGroup.Item key = {index}>{Object.values(ingredients)}</ListGroup.Item>)
+                        
+                    )}
+                </ListGroup>
+            </Card>
+
+            <Form>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>To add an ingredient or set of directions to this recipe, type the desired addition below and press UPDATE RECIPE.</Form.Label>
+                    <Form.Control as="textarea" rows={3} placeholder="1/2 Cup Something Sweet" onChange={handleFormControlChange}/>
+                </Form.Group>
+            </Form>
+
+            <Button variant="dark" onClick={handleButtonClick}>UPDATE RECIPE</Button>
+
         </>
 
 //         // Loop through each key in the itemsObject
