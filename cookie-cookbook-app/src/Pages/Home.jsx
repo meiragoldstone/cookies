0baec733997dabee42d93b4e847273f2ed6d7a01
Daniella Shoob <dshoob@wits.edu>
2024-07-03 17:06:04 -0400

    Home Branch with pics

--- cookie-cookbook-app/src/Pages/Home.jsx
+++ cookie-cookbook-app/src/Pages/Home.jsx
@@ -5,7 +5,11 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 
+
 const Home = () => {
+
+    const image1 = require("../Images/cookie.jpg")
+
     return (
         <Container>
             <h1 className="text-center mt-5">Cookie Cookbook</h1>
@@ -15,7 +19,9 @@ const Home = () => {
             <Row className="justify-content-center">
                 <Col xs={12} className="text-center">
                     <h2 className="mt-5">Find Your Favorite Recipes!</h2>
-                    <Image src="/cookie.jpg" fluid alt="cookie" style={{ maxWidth: '100%', height: 'auto' }} />
+                </Col>
+                <Col xs={12} md={6} className="text-center">
+                <Col><Image src = {image1} alt = "cookie.jpg" className = "img-fluid" width = "75%" height = "40%"/></Col>
                 </Col>
             </Row>
         </Container>
