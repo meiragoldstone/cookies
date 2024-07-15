ed19cc2ce99a1f3dd803114162b55ac81b581d51
Rivka Benyowitz <rbenyowitz@wits.edu>
2024-07-15 19:07:40 -0400

    Wrote retieve, update, and branch 33 changes. Still need to retireve all item keys in db

--- cookie-cookbook-app/src/Pages/Update.jsx
+++ cookie-cookbook-app/src/Pages/Update.jsx
@@ -9,102 +9,184 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Alert from 'react-bootstrap/Alert';
 
-const Update = () => {
-    const [recipeList, setRecipeList] = useState({})
-    const [chosenRecipe, setChosenRecipe] = useState({})
+const Update = () => {        
+    const [chosenRecipeData, setChosenRecipeData] = useState({Ingredients: "", Directions: ""});
     const [chosenRecipeId, setChosenRecipeId] = useState("");
-    const [inputValue, setInputValue] = useState("");
-    const [isIngredient, setIsIngredient] = useState("False");
+    const [newItem, setNewItem] = useState("");
     const [responseMessage, setResponseMessage] = useState({variant: "", message: ""});
+    const [isIngredient, setIsIngredient] = useState("False");
+    const [ingredientList, setIngredientList] = useState([]);
+    const [directionList, setDirectionList] = useState([]);
+    const [clickedPreview, setClickedPreview] = useState(false);
 
-    const handleSelectChange = (event) => {
-        const selectedKey = event.target.value;
-        const recipe = recipeList.find(item => Object.keys(item)[0] === selectedKey);
-        setChosenRecipe(recipe);
-    }
+    const retrieveRecipe = () => {
+        const recipe = chosenRecipeData?.map((item, index) => {
+            return <li key = {index}>{item}</li>
+        });
 
-    const handleFormControlChange = (event) => {
-        setInputValue(event.target.value);
-
-    }
-
-    const handleButtonPreview = (event) => {
-        if (inputValue.trim() !== ""){
-            const recipeName = Object.keys(chosenRecipe)[0];
-            const additionKey = Object.values(chosenRecipe[recipeName]).length + 1;
-
-            const newRecipeList = [...chosenRecipe[recipeName], {[additionKey]:inputValue}];
-            const updatedRecipe = {[recipeName]:newRecipeList};
-
-            setChosenRecipe(updatedRecipe);
+        function getRecipeByKey() {
+            const retrieveUrl = "https://lqtpgzkl41.execute-api.us-east-1.amazonaws.com/default/retrieveRecipe?listId=" + chosenRecipeId
+            fetch(retrieveUrl)
+                .then(response => response.json())
+                .then(data => {
+                    setChosenRecipeData({Ingredients:data.ingredients, Directions:data.directions})
+                    setIngredientList(chosenRecipeData.Ingredients);
+                    setDirectionList(chosenRecipeData.Directions);
+                });
         }
-
-        // clear form control after add the input
-        setInputValue("");
-
     }
 
-    const handleButtonSave = (event) => {
+    const updateRecipe = () => {
         setResponseMessage({});
-        const queryString = "?listId=" + chosenRecipeId;
-
+        
         const params = {
-            method: 'GET',
+            method: "POST",
             body: JSON.stringify({
                 isIngredient: isIngredient,
-                newItem: inputValue,
+                newItem: newItem
             })
         }
 
-        const apiUrl = "https://lqtpgzkl41.execute-api.us-east-1.amazonaws.com/default/updateRecipe" + queryString;
+        const updateUrl = "https://lqtpgzkl41.execute-api.us-east-1.amazonaws.com/default/updateRecipe?listId=" + chosenRecipeId;
 
-        fetch(apiUrl, params)
+        fetch(updateUrl, params)
             .then(response => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            return response.json();
-          })
-          .then(_ => {
-            setResponseMessage({variant: 'success', message: `${chosenRecipeId} was updated successfully`});
-          })
-          .catch(_ => {
-            setResponseMessage({variant: 'danger', message:`Error in updating ${chosenRecipeId}`});
-          });
+                if (!response.ok) {
+                    throw new Error("Network response was not ok.");
+                }
+                return response.json();
+            })
+
+            .then(_ => {
+                setResponseMessage({variant: 'success', message: `${chosenRecipeId} was updated successfully`});
+            })
+
+            .catch(_ => {
+                setResponseMessage({variant: 'danger', message:`Error in updating ${chosenRecipeId}`});
+            })
+    }
 
 
-        // const updatedRecipeList = recipeList.map(recipe => {
-        //     const recipeName = Object.keys(recipe)[0];
-        //     if (recipeName === Object.keys(chosenRecipe)[0]){
-        //         return chosenRecipe;
-        //     }
-        //     else{
-        //         return recipe;
-        //     }
-        // });
+    // const [recipeList, setRecipeList] = useState({})
+    // const [chosenRecipe, setChosenRecipe] = useState({})
+    // const [chosenRecipeId, setChosenRecipeId] = useState("");
+    // const [inputValue, setInputValue] = useState("");
+    // const [isIngredient, setIsIngredient] = useState("False");
+    // const [responseMessage, setResponseMessage] = useState({variant: "", message: ""});
 
-        // setRecipeList(updatedRecipeList);
+    const handleSelectChange = (event) => {
+        setChosenRecipeId(event.target.value);
+        retrieveRecipe();
+        
+        // const selectedKey = event.target.value;
+        // const recipe = recipeList.find(item => Object.keys(item)[0] === selectedKey);
+        // setChosenRecipe(recipe);
+    }
 
-        // setSaveSuccessMsg("The recipe for " + Object.keys(chosenRecipe)[0] + " was successfully updated and saved!");
-        // setTimeout(() => {
-        //     setSaveSuccessMsg("");
-        // }, 60000);
-
-
-        // updatedRecipeList.map(recipe => {
-        //     console.log(recipe);
-        // })
+    const handleFormControlChange = (event) => {
+        setNewItem((event.target.value).trim());
         
     }
+
+    const handleButtonPreview = (event) => {
+        if (newItem !== ""){
+            if (isIngredient == "True"){
+                setIngredientList(chosenRecipeData.Ingredients.concat(newItem)); 
+                setClickedPreview(true);
+            }else if (isIngredient == "False"){
+                setDirectionList(chosenRecipeData.Directions.concat(newItem)); 
+                setClickedPreview(true);
+            }else{
+                console.log("ERROR: MUST SPECIFY IF THIS IS AN INGREDIENT OR DIRECTION")
+            }
+        } 
+     }
+    // Old Handle Button Preview---------------------------------------------------
+    //     if (newItem !== ""){
+    //         // const recipeName = Object.keys(chosenRecipe)[0];
+    //         // const additionKey = Object.values(chosenRecipe[recipeName]).length + 1;
+
+    //         const newRecipeList = [...chosenRecipe[recipeName], {[additionKey]:inputValue}];
+    //         const updatedRecipe = {[recipeName]:newRecipeList};
+
+    //         setChosenRecipe(updatedRecipe);
+    //     }
+
+    //     // clear form control after add the input
+    //     setInputValue("");
+
+     const handleButtonSave = (event) => {
+        if (!clickedPreview){
+            handleButtonPreview();
+        }
+        setChosenRecipeData({Ingredients:ingredientList, Directions:directionList});
+        updateRecipe();
+     }
+
+    // const handleButtonSave = (event) => {
+    //     setResponseMessage({});
+
+    //     const params = {
+    //         method: 'GET',
+    //         body: JSON.stringify({
+    //             isIngredient: isIngredient,
+    //             newItem: inputValue,
+    //         })
+    //     }
+
+    //     const apiUrl = "https://lqtpgzkl41.execute-api.us-east-1.amazonaws.com/default/updateRecipe" + queryString;
+
+    //     fetch(apiUrl, params)
+    //         .then(response => {
+    //         if (!response.ok) {
+    //           throw new Error('Network response was not ok');
+    //         }
+    //         return response.json();
+    //       })
+    //       .then(_ => {
+    //         setResponseMessage({variant: 'success', message: `${chosenRecipeId} was updated successfully`});
+    //       })
+    //       .catch(_ => {
+    //         setResponseMessage({variant: 'danger', message:`Error in updating ${chosenRecipeId}`});
+    //       });
+
+
+    //     // const updatedRecipeList = recipeList.map(recipe => {
+    //     //     const recipeName = Object.keys(recipe)[0];
+    //     //     if (recipeName === Object.keys(chosenRecipe)[0]){
+    //     //         return chosenRecipe;
+    //     //     }
+    //     //     else{
+    //     //         return recipe;
+    //     //     }
+    //     // });
+
+    //     // setRecipeList(updatedRecipeList);
+
+    //     // setSaveSuccessMsg("The recipe for " + Object.keys(chosenRecipe)[0] + " was successfully updated and saved!");
+    //     // setTimeout(() => {
+    //     //     setSaveSuccessMsg("");
+    //     // }, 60000);
+
+
+    //     // updatedRecipeList.map(recipe => {
+    //     //     console.log(recipe);
+    //     // })
+        
+    // }
 
 
     return (
         <>
             <Container>
+                <Row className = "m-5 text-center">
+                    <Col><h1>Update</h1></Col>
+                </Row>
+
                 <Row className = "m-5">
                     <Col className = "m-2">
                         <Form.Select aria-label="Default select example" size="lg" onChange = {(event) => handleSelectChange(event)}>
-                            <option disabled>Choose A Recipe</option>
+                            <option disabled selected>Choose A Recipe</option>
                             {recipeList.map((recipe, index) => (
                                 <option key = {index} value={Object.keys(recipe)[0]} >{Object.keys(recipe)[0]}</option>)    
                             )}
@@ -132,7 +214,7 @@ const Update = () => {
                                     <Form>
                                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                                             <Form.Label>To add an ingredient or set of directions to this recipe, type the desired addition below, preview your changes and then click SAVE CHANGES.</Form.Label>
-                                            <Form.Control as="textarea" rows={3} placeholder="1/2 Cup Something Sweet" onChange={handleFormControlChange} value = {inputValue}/>
+                                            <Form.Control as="textarea" rows={3} placeholder="Enter your addition here" onChange={handleFormControlChange} value = {inputValue}/>
                                         </Form.Group>
                                     </Form>
                                 </Col>
