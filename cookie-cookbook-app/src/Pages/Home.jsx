3747144fe073c9fc976521ffc4158b3e8bfcc956
Daniella Shoob <dshoob@wits.edu>
2024-07-17 22:55:20 -0400

    Retrieve Page Connected to Database

--- cookie-cookbook-app/src/Pages/Home.jsx
+++ cookie-cookbook-app/src/Pages/Home.jsx
@@ -7,18 +7,15 @@ import Image from 'react-bootstrap/Image';
 
 const Home = () => {
 
-    const image1 = require("../Images/colorful-cookies.jpg")
+    const image1 = require("../Images/cookie.jpg")
 
     return (
         <Container>
             <h1 className="text-center mt-5">Cookie Cookbook</h1>
-            <p className="lead text-center">
-                This is the home page of the cookie cookbook website.
-            </p>
             <Row className="justify-content-center">
                 <Col xs={12} className="text-center">
                     <h2 className="mt-5">Find Your Favorite Recipes!</h2>
-                    <Col><Image src = {image1} alt = "colorful-cookies.jpg" className = "img-fluid" width = "75%" height = "40%"/></Col>
+                    <Col><Image src = {image1} alt = "cookie.jpg" className = "img-fluid" width = "75%" height = "40%"/></Col>
                 </Col>
             </Row>
         </Container>
