bea4d644b76580c483bec6f6c3b09aee0173ab66
Daniella Shoob <dshoob@wits.edu>
2024-07-21 10:06:02 -0400

    new merge try

--- cookie-cookbook-app/src/Pages/Home.jsx
+++ cookie-cookbook-app/src/Pages/Home.jsx
@@ -5,6 +5,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 
+
 const Home = () => {
 
     const image1 = require("../Images/cookie.jpg")
@@ -12,10 +13,15 @@ const Home = () => {
     return (
         <Container>
             <h1 className="text-center mt-5">Cookie Cookbook</h1>
+            <p className="lead text-center">
+                This is the home page of the cookie cookbook website.
+            </p>
             <Row className="justify-content-center">
                 <Col xs={12} className="text-center">
                     <h2 className="mt-5">Find Your Favorite Recipes!</h2>
-                    <Col><Image src = {image1} alt = "cookie.jpg" className = "img-fluid" width = "75%" height = "40%"/></Col>
+                </Col>
+                <Col xs={12} md={6} className="text-center">
+                <Col><Image src = {image1} alt = "cookie.jpg" className = "img-fluid" width = "75%" height = "40%"/></Col>
                 </Col>
             </Row>
         </Container>
